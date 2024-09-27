f93ad37a40ca2eb7a4c4d7e29e93c51235ca9750
Otávio Grassi <ramosgrassi@gmail.com>
2024-09-27 15:48:08 -0300

    adição da página inicial

--- javascripts/events.js
+++ javascripts/events.js
@@ -19,4 +19,60 @@ $(document).ready(function(){
 
   // Seu código abaixo
 
+  document.getElementById('iniciar').addEventListener('click', function() {
+    // Limpa a tela inicial
+   home.classList.add('hidden-div');
+   menu.classList.remove('hidden-div');
+});
+
+  //  Criação das variáveis 
+  let data;
+  var num = 0;
+  let iniciar = document.getElementById('iniciar');
+  let home = document.getElementById('home');
+  let menu = document.getElementById('menu');
+
+
+  // Função para carregar o JSON
+  async function loadJSON() {
+      try {
+          const response = await fetch('data.json');
+          if (!response.ok) {
+              throw new Error('Network response was not ok');
+          }
+          data = await response.json();
+          // Carregar a página inicial ao abrir
+          //loadPage('home');
+      } catch (error) {
+          console.error('Erro ao carregar o JSON:', error);
+      }
+  }
+
+  function loadPage(page) {
+      const pageData = data.pages[page];
+      if (pageData) {
+          document.getElementById('teste').innerText = pageData.title;
+
+          // Limpar o conteúdo anterior
+          const contentDiv = document.getElementById('teste');
+          contentDiv.innerHTML = ''; // Limpa o conteúdo anterior
+
+          // Adicionar todos os textos do array content como parágrafos
+          pageData.content.forEach(text => {
+              const paragraph = document.createElement('p') // Cria o paragrafo
+              paragraph.className = `${page}`; // Adiciona a classe ao paragrafo 
+              paragraph.innerHTML = text; // Define o texto do parágrafo
+              contentDiv.appendChild(paragraph); // Adiciona o parágrafo ao div
+          });
+      }
+  }
+
+  // Chamar a função para carregar o JSON
+  loadJSON();
+
+  // Adicionar funções aos botões
+
+  iniciar.addEventListener('click', function () {
+    RenderizarTexto();
+  });
 })
